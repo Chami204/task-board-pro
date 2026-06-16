187adbcd3ca486026de67aeaa00f35023e6d8e05
Chami204 <chamidimanthi@gmail.com>
2026-06-16 15:40:11 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -16,31 +16,21 @@ SHEET_ID = "1P1f1rW4l1a_hRUGZMJdhkpm7PKenbBFKvV6p5rRjXPs"
 sheet = client.open_by_key(SHEET_ID).sheet1
 
 
-# ----------------------
-# INIT HEADERS (AUTO)
-# ----------------------
+# -----------------------
+# INIT HEADERS
+# -----------------------
 def init_sheet():
-    expected = [
-        "id", "name", "date", "start", "end",
-        "hours", "technician", "assigned_by", "color"
-    ]
-
-    existing = sheet.row_values(1)
-
-    if not existing or existing != expected:
-        sheet.clear()
-        sheet.append_row(expected)
+    if sheet.row_count == 0:
+        sheet.append_row([
+            "id", "name", "date", "start", "end",
+            "hours", "technician", "assigned_by", "color"
+        ])
 
 
-# ----------------------
-# GET DATA
-# ----------------------
 def get_all():
-    return sheet.get_all_records()
+    data = sheet.get_all_records()
+    return data
 
 
-# ----------------------
-# APPEND ROW
-# ----------------------
 def append_row(row):
     sheet.append_row(row)
